4efc353401fca275dcdf87a9c45eb5d34c36177b
Armin Reichert <armin.reichert@web.de>
2020-03-02 09:24:45 +0100

    extended

--- MazeDemos/src/main/java/de/amr/demos/grid/maze/eight/EightNeighborsMazeApp.java
+++ MazeDemos/src/main/java/de/amr/demos/grid/maze/eight/EightNeighborsMazeApp.java
@@ -1,5 +1,7 @@
 package de.amr.demos.grid.maze.eight;
 
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.Random;
 
 import javax.swing.JFrame;
@@ -12,9 +14,13 @@ import de.amr.graph.grid.impl.GridFactory;
 import de.amr.graph.grid.impl.GridGraph;
 import de.amr.graph.grid.ui.rendering.GridCanvas;
 import de.amr.graph.grid.ui.rendering.PearlsGridRenderer;
+import de.amr.graph.pathfinder.util.GraphSearchUtils;
 import de.amr.graph.util.GraphUtils;
 import de.amr.maze.alg.core.MazeGenerator;
-import de.amr.maze.alg.others.RecursiveDivision;
+import de.amr.maze.alg.mst.KruskalMST;
+import de.amr.maze.alg.others.BinaryTree;
+import de.amr.maze.alg.others.Eller;
+import de.amr.maze.alg.others.HuntAndKill;
 import de.amr.maze.alg.traversal.IterativeDFS;
 import de.amr.maze.alg.traversal.RandomBFS;
 import de.amr.maze.alg.ust.WilsonUSTRecursiveCrosses;
@@ -28,31 +34,47 @@ public class EightNeighborsMazeApp {
 	GridGraph<TraversalState, Integer> grid;
 	MazeGenerator gen;
 	GridCanvas canvas;
+	volatile boolean next = true;
 
 	public EightNeighborsMazeApp() {
 		SwingUtilities.invokeLater(this::showUI);
 	}
 
-	void maze() {
+	void grid() {
 		grid = GridFactory.emptyGrid(20, 20, Grid8Topology.get(), TraversalState.UNVISITED, 0);
+	}
+
+	void maze() {
 		int center = grid.cell(GridPosition.CENTER);
-		int choice = new Random().nextInt(4);
+		int choice = new Random().nextInt(7);
 		switch (choice) {
 		case 0:
 			System.out.println("DFS");
 			gen = new IterativeDFS(grid);
 			break;
 		case 1:
-			System.out.println("Wilson");
-			gen = new WilsonUSTRecursiveCrosses(grid);
-			break;
-		case 2:
 			System.out.println("BFS");
 			gen = new RandomBFS(grid);
 			break;
+		case 2:
+			System.out.println("Wilson");
+			gen = new WilsonUSTRecursiveCrosses(grid);
+			break;
 		case 3:
-			System.out.println("Recursive Division");
-			gen = new RecursiveDivision(grid);
+			System.out.println("Kruskal");
+			gen = new KruskalMST(grid);
+			break;
+		case 4:
+			System.out.println("Eller");
+			gen = new Eller(grid);
+			break;
+		case 5:
+			System.out.println("Binary Tree");
+			gen = new BinaryTree(grid);
+			break;
+		case 6:
+			System.out.println("Hunt and Kill");
+			gen = new HuntAndKill(grid);
 			break;
 		default:
 			System.out.println("DFS");
@@ -60,11 +82,29 @@ public class EightNeighborsMazeApp {
 			break;
 		}
 		gen.createMaze(grid.col(center), grid.row(center));
-		if (GraphUtils.containsCycle(grid)) {
-			System.out.println("No maze! Cycle detected.");
-		} else {
+		if (isMaze()) {
 			System.out.println("Maze created.");
+		} else {
+			System.out.println("No maze!");
 		}
+	}
+
+	boolean isMaze() {
+		boolean edgeCountOk = grid.numEdges() == grid.numVertices() - 1;
+		if (!edgeCountOk) {
+			System.out.println("Edge count failed " + grid.numEdges() + ", should be " + (grid.numVertices() - 1));
+		} else {
+			System.out.println("Edge count ok: " + grid.numEdges());
+		}
+		boolean cycleFree = !GraphUtils.containsCycle(grid);
+		if (!cycleFree) {
+			System.out.println("Cycle detected");
+		}
+		boolean connected = GraphSearchUtils.isConnectedGraph(grid);
+		if (!connected) {
+			System.out.println("Disconnected graph");
+		}
+		return edgeCountOk && connected && cycleFree;
 	}
 
 	void render() {
@@ -78,6 +118,14 @@ public class EightNeighborsMazeApp {
 		f.setTitle("Maze from grid with 8-neighbor topology");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setResizable(false);
+		f.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+					next = true;
+				}
+			}
+		});
 		canvas = new GridCanvas();
 		f.getContentPane().add(canvas);
 		PearlsGridRenderer gr = new PearlsGridRenderer();
@@ -85,7 +133,7 @@ public class EightNeighborsMazeApp {
 		gr.fnCellSize = () -> 32;
 		gr.fnPassageWidth = (u, v) -> 4;
 		canvas.pushRenderer(gr);
-		maze();
+		grid();
 		render();
 		f.pack();
 		f.setVisible(true);
@@ -94,10 +142,14 @@ public class EightNeighborsMazeApp {
 
 	void loop() {
 		while (true) {
-			maze();
-			render();
+			if (next) {
+				grid();
+				maze();
+				render();
+				next = false;
+			}
 			try {
-				Thread.sleep(1000);
+				Thread.sleep(100);
 			} catch (InterruptedException x) {
 				x.printStackTrace();
 			}
