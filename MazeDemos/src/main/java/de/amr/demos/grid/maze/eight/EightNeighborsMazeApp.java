312339710b6e8b7b29f19d9d8d19eef5bd02c3ff
Armin Reichert <armin.reichert@web.de>
2020-03-03 09:26:01 +0100

    improved

--- MazeDemos/src/main/java/de/amr/demos/grid/maze/eight/EightNeighborsMazeApp.java
+++ MazeDemos/src/main/java/de/amr/demos/grid/maze/eight/EightNeighborsMazeApp.java
@@ -3,12 +3,14 @@ package de.amr.demos.grid.maze.eight;
 import java.awt.Color;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 import de.amr.graph.core.api.TraversalState;
+import de.amr.graph.grid.api.GridGraph2D;
 import de.amr.graph.grid.api.GridPosition;
 import de.amr.graph.grid.impl.Grid8Topology;
 import de.amr.graph.grid.impl.GridFactory;
@@ -25,12 +27,37 @@ import de.amr.maze.alg.others.Eller;
 import de.amr.maze.alg.others.HuntAndKill;
 import de.amr.maze.alg.traversal.IterativeDFS;
 import de.amr.maze.alg.traversal.RandomBFS;
-import de.amr.maze.alg.ust.WilsonUSTRecursiveCrosses;
+import de.amr.maze.alg.ust.WilsonUSTRandomCell;
+
+class Generator {
+	String name;
+	Class<?> genClass;
+
+	static Generator of(String name, Class<?> genClass) {
+		Generator gen = new Generator();
+		gen.name = name;
+		gen.genClass = genClass;
+		return gen;
+	}
+}
 
 public class EightNeighborsMazeApp {
 
-	static final int GRID_SIZE = 40;
+	static final int GRID_SIZE = 80;
 	static final int CANVAS_SIZE = 640;
+
+	static final Generator[] GEN = {
+		//@formatter:off
+		Generator.of("DFS", IterativeDFS.class), 
+		Generator.of("BFS", RandomBFS.class),
+		Generator.of("Kruskal", KruskalMST.class), 
+		Generator.of("Prim", PrimMST.class),
+		Generator.of("Wilson", WilsonUSTRandomCell.class), 
+		Generator.of("Eller", Eller.class),
+		Generator.of("BinaryTree", BinaryTree.class), 
+		Generator.of("HuntAndKill", HuntAndKill.class), 
+		//@formatter:on
+	};
 
 	public static void main(String[] args) {
 		new EightNeighborsMazeApp();
@@ -49,53 +76,33 @@ public class EightNeighborsMazeApp {
 		grid = GridFactory.emptyGrid(GRID_SIZE, GRID_SIZE, Grid8Topology.get(), TraversalState.UNVISITED, 0);
 	}
 
+	void mazes() {
+		for (int i = 0; i < 60; ++i) {
+			System.out.println("#" + i);
+			maze();
+			render();
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException x) {
+				x.printStackTrace();
+			}
+		}
+	}
+
 	void maze() {
 		grid();
 		int center = grid.cell(GridPosition.CENTER);
-		int choice = new Random().nextInt(8);
-		switch (choice) {
-		case 0:
-			System.out.println("DFS");
-			gen = new IterativeDFS(grid);
-			break;
-		case 1:
-			System.out.println("BFS");
-			gen = new RandomBFS(grid);
-			break;
-		case 2:
-			System.out.println("Wilson");
-			gen = new WilsonUSTRecursiveCrosses(grid);
-			break;
-		case 3:
-			System.out.println("Kruskal");
-			gen = new KruskalMST(grid);
-			break;
-		case 4:
-			System.out.println("Eller");
-			gen = new Eller(grid);
-			break;
-		case 5:
-			System.out.println("Binary Tree");
-			gen = new BinaryTree(grid);
-			break;
-		case 6:
-			System.out.println("Hunt and Kill");
-			gen = new HuntAndKill(grid);
-			break;
-		case 7:
-			System.out.println("Prim");
-			gen = new PrimMST(grid);
-			break;
-		default:
-			System.out.println("DFS");
-			gen = new IterativeDFS(grid);
-			break;
-		}
-		gen.createMaze(grid.col(center), grid.row(center));
-		if (isMaze()) {
-			System.out.println("Maze created.");
-		} else {
-			System.out.println("No maze!");
+		int choice = new Random().nextInt(GEN.length);
+		try {
+			gen = (MazeGenerator) GEN[choice].genClass.getConstructor(GridGraph2D.class).newInstance(grid);
+			System.out.println(GEN[choice].name);
+			gen.createMaze(grid.col(center), grid.row(center));
+			if (!isMaze()) {
+				System.out.println("No maze!");
+			}
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+				| NoSuchMethodException | SecurityException x) {
+			x.printStackTrace();
 		}
 	}
 
@@ -103,8 +110,6 @@ public class EightNeighborsMazeApp {
 		boolean edgeCountOk = grid.numEdges() == grid.numVertices() - 1;
 		if (!edgeCountOk) {
 			System.out.println("Edge count failed " + grid.numEdges() + ", should be " + (grid.numVertices() - 1));
-		} else {
-			System.out.println("Edge count ok: " + grid.numEdges());
 		}
 		boolean cycleFree = !GraphUtils.containsCycle(grid);
 		if (!cycleFree) {
@@ -119,7 +124,6 @@ public class EightNeighborsMazeApp {
 
 	void render() {
 		canvas.setGrid(grid);
-		canvas.clear();
 		canvas.drawGrid();
 	}
 
@@ -137,30 +141,18 @@ public class EightNeighborsMazeApp {
 			}
 		});
 		canvas = new GridCanvas();
+		canvas.setCellSize(CANVAS_SIZE / GRID_SIZE);
 		f.getContentPane().add(canvas);
 		PearlsGridRenderer gr = new PearlsGridRenderer();
-		gr.fnPassageColor = (u,v) -> Color.YELLOW;
+		gr.fnPassageColor = (u, v) -> Color.YELLOW;
 		gr.fnRelativePearlSize = () -> .25;
-		gr.fnCellSize = () -> CANVAS_SIZE / GRID_SIZE;
+		gr.fnCellSize = () -> canvas.getCellSize();
 		gr.fnPassageWidth = (u, v) -> 2;
 		canvas.pushRenderer(gr);
-		canvas.setCellSize(CANVAS_SIZE / GRID_SIZE);
 		grid();
 		render();
 		f.pack();
 		f.setVisible(true);
 		new Thread(this::mazes).start();
-	}
-
-	void mazes() {
-		for (int i = 0; i < 1000; ++i) {
-			maze();
-			render();
-			try {
-				Thread.sleep(10);
-			} catch (InterruptedException x) {
-				x.printStackTrace();
-			}
-		}
 	}
 }
