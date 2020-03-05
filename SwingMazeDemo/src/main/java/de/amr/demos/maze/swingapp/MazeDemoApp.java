56f6326f6f4bbe56f7f45043f460cfb2f5099619
Armin Reichert <armin.reichert@web.de>
2020-03-05 12:44:32 +0100

    cleanup

--- SwingMazeDemo/src/main/java/de/amr/demos/maze/swingapp/MazeDemoApp.java
+++ SwingMazeDemo/src/main/java/de/amr/demos/maze/swingapp/MazeDemoApp.java
@@ -2,10 +2,10 @@ package de.amr.demos.maze.swingapp;
 
 import static de.amr.swing.Swing.action;
 
+import java.awt.Dimension;
 import java.awt.EventQueue;
 
 import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 
 import com.beust.jcommander.JCommander;
@@ -24,46 +24,59 @@ import de.amr.swing.Swing;
  * This application visualizes different maze generation algorithms and path
  * finders.
  * <p>
- * The application provides a (by default full-screen, undecorated) grid display
- * area where the maze generation and path finding animations are shown.
- * <p>
- * A control window allows changing the generation and path finder algorithm,
- * changing the resolution of the grid, the rendering style and other settings.
+ * The application provides
+ * <ul>
+ * <li>a preview window (full-screen by default) where the maze generation and
+ * path finding animations are shown.
+ * <li>a control window where the following settings can be made
+ * <ul>
+ * <li>the maze generation algorithm
+ * <li>the pathfinder algorithm
+ * <li>the resolution of the grid
+ * <li>the topology of the grid
+ * <li>the rendering style and some other settings settings.
+ * </ul>
+ * </ul>
  * 
  * @author Armin Reichert
  */
 public class MazeDemoApp {
-
-	@Parameter(description = "Theme class name (or: 'system', 'cross', 'metal', 'nimbus')", names = { "-laf",
-			"-theme" }, converter = ThemeConverter.class)
-	private String theme = NimbusLookAndFeel.class.getName();
-
-	@Parameter(description = "Grid window width", names = { "-width" })
-	private int windowWidth = Swing.getDisplaySize().width;
-
-	@Parameter(description = "Grid window height", names = { "-height" })
-	private int windowHeight = Swing.getDisplaySize().height;
-
-	private final MazeDemoModel model = new MazeDemoModel();
 
 	public static void main(String[] args) {
 		MazeDemoApp theApp = new MazeDemoApp();
 		JCommander argsParser = JCommander.newBuilder().addObject(theApp).build();
 		argsParser.usage();
 		argsParser.parse(args);
+		try {
+			UIManager.setLookAndFeel(theApp.theme);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		EventQueue.invokeLater(theApp::createAndShowUI);
 	}
 
+	@Parameter(description = "Grid window width", names = { "-width" })
+	private int windowWidth;
+
+	@Parameter(description = "Grid window height", names = { "-height" })
+	private int windowHeight;
+
+	@Parameter(description = "Theme class name (or: 'system', 'cross', 'metal', 'nimbus')", names = { "-laf",
+			"-theme" }, converter = ThemeConverter.class)
+	private String theme;
+
+	private final MazeDemoModel model;
+
+	public MazeDemoApp() {
+		Dimension displaySize = Swing.getDisplaySize();
+		windowWidth = displaySize.width;
+		windowHeight = displaySize.height;
+		theme = NimbusLookAndFeel.class.getName();
+		model = new MazeDemoModel();
+	}
+
 	private void createAndShowUI() {
-		try {
-			UIManager.setLookAndFeel(theme);
-		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
-				| UnsupportedLookAndFeelException e) {
-			e.printStackTrace();
-		}
-
 		GridUI gridUI = new GridUI(model, windowWidth, windowHeight);
-
 		ControlUI controlUI = new ControlUI(gridUI);
 		controlUI.setBusy(false);
 		controlUI.setHiddenWhenBusy(false);
@@ -72,9 +85,7 @@ public class MazeDemoApp {
 		controlUI.placeWindowRelativeTo(gridUI.getWindow());
 		model.findGenerator(RandomBFS.class).ifPresent(controlUI::selectGenerator);
 		model.findSolver(AStarSearch.class).ifPresent(controlUI::selectSolver);
-
 		gridUI.setEscapeAction(action("Escape", e -> controlUI.show()));
-
 		gridUI.show();
 		controlUI.show();
 	}
